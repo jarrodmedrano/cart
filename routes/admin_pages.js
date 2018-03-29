d3f5c14b82fc98a25abaeae80b141b7af498123c
jarrod <jmedran@gmail.com>
2018-03-29 16:33:27 -0600

    submitting page

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+//Get page model
+var Page = require('../models/page');
+
 /*
     * GET pages index
  */
@@ -50,7 +53,24 @@ router.post('/add-page', function(req, res) {
             content: content
         });
     } else {
-        console.log('success');
+        Page.findOne({slug: slug}, function(err, page) {
+            if(page) {
+                req.flash('danger', 'Page slug exists choose another');
+            } else {
+                var page = new Page({
+                    title: title,
+                    slug: slug,
+                    content: content,
+                    sorting: 0
+                });
+
+                page.save(function(err) {
+                    if(err) return console.log(err);
+                    req.flash('success', 'Page added!')
+                    res.redirect('/admin/pages');
+                });
+            }
+        })
     }
 
 });
